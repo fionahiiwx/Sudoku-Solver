b78e1e6556ae2acc79f1e02e9f93abf11ba1b8dc
Sparklez_Foxy <fionahii@outlook.my>
2023-06-06 21:35:49 +0800

    sudoku solvable, some style

--- code/script.js
+++ code/script.js
@@ -1,56 +1,3 @@
-// guidlines
-// 1. Input Validation: 
-//  Before solving the Sudoku puzzle, you can add a validation step 
-//  to ensure that the user-entered values are valid. Check if the 
-//  input follows the rules of Sudoku, such as having unique numbers
-//  in each row, column, and 3x3 subgrid.
-
-// 2. Grid Update: 
-//  Modify the updateGrid function to handle input validation and 
-//  update the grid accordingly. You can display error messages or 
-//  highlight incorrect input to provide feedback to the user.
-
-// 3. Algorithm Integration: 
-//  Integrate the Sudoku solving algorithm into the solve Sudoku 
-//  function. You can implement a backtracking algorithm in 
-//  JavaScript to solve the puzzle. Consider using recursion and 
-//  backtracking to explore different possibilities until a valid 
-//  solution is found.
-
-// 4. Solver Visualization: 
-//  To provide a visual representation of the solving process, 
-//  you can add delay between each step of the solver algorithm. 
-//  Use setTimeout or setInterval functions to introduce a pause 
-//  between each recursive call. This will allow the user to see 
-//  the solving progress on the grid.
-
-// 5. Solution Display: 
-//  After solving the puzzle, update the grid cells with the solved 
-//  values in the solveSudoku function. You can change the text 
-//  color and background color to distinguish the solved cells from 
-//  the user input cells.
-
-// 6. Multiple Solutions: 
-//  If you want to handle cases where there are multiple valid 
-//  solutions, you can modify the solver algorithm to find and 
-//  display all possible solutions. This could involve tracking 
-//  and backtracking through different branches of the solution 
-//  space.
-
-// 7. Error Handling: 
-//  Implement proper error handling and messages for edge cases or 
-//  invalid input, such as when the solver algorithm fails to find 
-//  a valid solution or the input puzzle is unsolvable.
-
-// 8. User Interactions: 
-//  Consider adding additional functionalities like a "Clear" 
-//  button to reset the grid or the ability for users to edit 
-//  their input after solving.
-
-
-
-
-
 // Create initial empyty grid
 let grid = Array.from({length: 9}, () => Array({length: 9}, () => 0));
 
@@ -86,15 +33,14 @@ function updateGrid() {
     for (let i = 0; i < 9; i++) {
         for (let j = 0; j < 9; j++) {
             const cell = document.getElementById(`cell-${i}-${j}`);
+            // puts 0 if no number is entered in that cell
             const userInput = parseInt(cell.innerText) || 0;
             const isConflict = checkConflict(i, j, userInput);
             
             if (isConflict) {
                 cell.style.backgroundColor = "red";
-                document.getElementById("error").innerHTML = "red";
             } else {
                 cell.style.backgroundColor = "white";
-                document.getElementById("error").innerHTML = "white";
             }
 
             grid[i][j] = userInput;
@@ -103,17 +49,10 @@ function updateGrid() {
 }
 
 function checkConflict(row, col, value) {
-    // Check for conflicts in the same row
-    for (let j = 0; j < 9; j++) {
-        if (j !== col && grid[row][j] === userInput) {
-            return true;
-        }
-    }
-
-    // Check for conflicts in the same column
+    // Check for conflicts in the same row or column
     for (let i = 0; i < 9; i++) {
-        if (i !== row && grid[i][col] === userInput) {
-            return true;
+        if (value !== 0 && ((i !== col && grid[row][i] === value) || (i !== row && grid[i][col] === value))) {
+            return true; // Conflict found
         }
     }
 
@@ -122,58 +61,133 @@ function checkConflict(row, col, value) {
     const startCol = Math.floor(col / 3) * 3;
     for (let i = startRow; i < startRow + 3; i++) {
         for (let j = startCol; j < startCol + 3; j++) {
-            if ((i !== row || j !== col) && grid[i][j] === userInput) {
-                return true;
+            if (value !== 0 && ((i !== row || j !== col) && grid[i][j] === value)) {
+                return true; // Conflict found
             }
         }
     }
 
-    return false;
-
-    // Checks Value in Row
-    // function isValueInRow(row, value) {
-    //     const rowValues = grid[row];
-    //     return rowValues.includes(value);
-    // }
-    // // Checks Value in Col
-    // function isValueInCol(col, value) {
-    //     return grid.some(row => row[col] === value);
-    // }
-    // // Checks Value in Subgrid
-    // function isValueInSubgrid(row, col, value) {
-    //     const subgridRowStart = Math.floor(row/3) * 3;
-    //     const subgridColStart = Math.floow(col/3) * 3;
-    //     for (let i=0; i<3; i++) {
-    //         for (let j=0; j<3; j++) {
-    //             if (grid[subgridRowStart+i][subgridColStart+j]===value) {
-    //                 return true;
-    //             }
-    //         }
-    //     }
-    //     return false
-    // }
-
-    
+    return false; // No conflict found
 }
 
 // -----
 // Solve
 // -----
-// Function to solve the Sudoku puzzle using backtracking
+// Function to solve the Sudoku puzzle using backtracking//
 function solveSudoku() {
-    // Implementation of Sudoku solver algorithm in Python/Pygame
+    // Start solving from the top-left cell
+    const startingRow = 0;
+    const startingCol = 0;
 
+    // Call the recursive solve function
+    if (solveRecursive(startingRow, startingCol)) {
+        // Puzzle solved successfully
+        console.log("Sudoku puzzle solved!");
+        displaySolution();
+    } else {
+        // Puzzle cannot be solved
+        console.log("No solution found for the Sudoku puzzle.");
+    }
+}
 
-    // Display the Solved Grid
-    for (let i=0; i<9; i++) {
-        for (let j=0; j<9; j++) {
+// Recursive function to solve the Sudoku puzzle
+function solveRecursive(row, col) {
+    // Base case: If all cells have been filled, the puzzle is solved
+    if (row === 9) {
+        return true;
+    }
+
+    // Calculate the next row and column indices
+    let nextRow = row;
+    let nextCol = col + 1;
+    if (nextCol === 9) {
+        // Move to the next row if the end of the current row is reached
+        nextRow = row + 1;
+        nextCol = 0;
+    }
+
+    // If the current cell is not empty, move to the next cell
+    if (grid[row][col] !== 0) {
+        return solveRecursive(nextRow, nextCol);
+    }
+
+    // Try different numbers in the current cell
+    for (let num = 1; num <= 9; num++) {
+        // Check if the number is valid in the current cell
+        if (isValidMove(row, col, num)) {
+            // Assign the number to the current cell
+            grid[row][col] = num;
+
+            // Recursively solve the puzzle starting from the next cell
+            if (solveRecursive(nextRow, nextCol)) {
+                return true; // Puzzle solved
+            }
+
+            // If the puzzle cannot be solved with the current number, backtrack
+            grid[row][col] = 0; // Reset the current cell
+        }
+    }
+
+    // No valid number found for the current cell, backtrack
+    return false;
+}
+
+// Function to check if a number can be placed in a cell without conflicts
+function isValidMove(row, col, num) {
+    // Check for conflicts in the same row or column
+    for (let i = 0; i < 9; i++) {
+        if (grid[row][i] === num || grid[i][col] === num) {
+            return false; // Conflict found
+        }
+    }
+
+    // Check for conflicts in the same 3x3 subgrid
+    const startRow = Math.floor(row / 3) * 3;
+    const startCol = Math.floor(col / 3) * 3;
+    for (let i = startRow; i < startRow + 3; i++) {
+        for (let j = startCol; j < startCol + 3; j++) {
+            if (grid[i][j] === num) {
+                return false; // Conflict found
+            }
+        }
+    }
+
+    return true; // No conflict found
+}
+
+// Display the solved Sudoku grid
+function displaySolution() {
+    const gridContainer = document.getElementById("grid");
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
             const cell = document.getElementById(`cell-${i}-${j}`);
-            cell.innerText = solvedGrid[i][j];
-            cell.style.color = "black";
-            cell.style.backgroundColor = "lightgreen"
+            const userInput = parseInt(cell.innerText) || 0;
+            if (userInput !== grid[i][j]) {
+                cell.innerText = grid[i][j].toString();
+                cell.style.color = "blue";
+                cell.style.backgroundColor = "lightgreen";
+            }
         }
     }
 }
 
-// Display Solution
-// function displaySolution {}
+// -----
+// Clear
+// -----
+// Get the clear button element
+const clearButton = document.getElementById("clearButton");
+// Add a click event listener to the clear button
+clearButton.addEventListener("click", clearGrid);
+
+// Function to clear the Sudoku grid
+function clearGrid() {
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            const cell = document.getElementById(`cell-${i}-${j}`);
+            cell.innerText = ""; // Clear the cell content
+            cell.style.color = "black"; // Reset the cell color
+            cell.style.backgroundColor = "white"; // Reset the cell background color
+            grid[i][j] = 0; // Reset the grid value
+        }
+    }
+}
